110be97a33833676b5df8c05cfa49b0e23552c04
bishoe01 <bishoe01@gmail.com>
2022-12-04 23:01:23 +0900

    tab todo complete or not completed

--- src/components/TodoList/Main.jsx
+++ src/components/TodoList/Main.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { ChakraProvider } from '@chakra-ui/react'
-import { Card, CardHeader, TabPanel,TabPanels,Divider, Tabs, Tab, TabList, IconButton, CardBody, CardFooter, Heading, Text, Box, Stack, StackDivider } from '@chakra-ui/react'
+import {
+    Card, CardHeader, Input,
+    FormControl,
+    FormLabel,
+    FormHelperText,
+    Button,
+    TabPanel, TabPanels, Checkbox, Divider, Tabs, Tab, TabList, IconButton, CardBody, CardFooter, Heading, Text, Box, Stack, StackDivider
+} from '@chakra-ui/react'
 import { SunIcon } from '@chakra-ui/icons';
 import '../../Todo.scss'
 import { BsFillBrightnessHighFill } from 'react-icons/bs';
 
 function Main(props) {
+    const [show , setShow] = useState([1,2,3]);
+    const [now , setNow] = useState(1);
+    const [todos, setTodos] = useState([]);
+    const onPressEnter = (e) => {
+        if (e.key === 'Enter') {
+            console.log(`${e.target.value} is added`);
+            setTodos([...todos, { text: e.target.value, isCompleted: false }]);
+            e.target.value = '';
+        }
+    }
+    useEffect(() => {
+        localStorage.setItem('todos', todos);
+    }, [todos])
+
     return (
         <ChakraProvider>
             <Card w="25%">
@@ -14,43 +35,66 @@ function Main(props) {
                         <IconButton colorScheme="gray" aria-label='Search database' icon={<SunIcon />} onClick={() => console.log("hi")} />
                         <Tabs>
                             <TabList>
-                                <Tab onClick={()=>{console.log("one");}}>One</Tab>
-                                <Tab>Two</Tab>
-                                <Tab>Three</Tab>
+                                {show.map((item, index) => {
+                                    return (
+                                        <Tab key={index} onClick={() => setNow(item)}>{item}</Tab>
+                                    )}
+                                )}
                             </TabList>
                         </Tabs>
 
                     </Heading>
-                    <Divider variant="dashed" mt="20px" style={{border:"0.2px solid gray"}}/>
+                    <Divider variant="dashed" mt="20px" style={{ border: "0.2px solid gray" }} />
                 </CardHeader>
                 <CardBody>
                     <Stack divider={<StackDivider />} spacing='4'>
-                        <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                Summary
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                View a summary of all your clients over the last month.
-                            </Text>
-                        </Box>
-                        <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                Overview
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                Check out the overview of your clients.
-                            </Text>
-                        </Box>
-                        <Box>
-                            <Heading size='xs' textTransform='uppercase'>
-                                Analysis
-                            </Heading>
-                            <Text pt='2' fontSize='sm'>
-                                See a detailed analysis of all your business clients.
-                            </Text>
-                        </Box>
+                        {
+                            now === 1 ? todos.map((todo,index) => {
+                                return (
+                                    <Box key={index} p="2" shadow="md" borderWidth="1px" borderRadius="md" style={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Text>{todo.text}</Text>
+                                        <Checkbox onChange={() => {
+                                            let newTodos = [...todos];
+                                            newTodos[index].isCompleted = !newTodos[index].isCompleted;
+                                            setTodos(newTodos);
+                                        }} />
+                                    </Box>
+                                )
+                            }) : (now===2) ? todos.filter((todo) => todo.isCompleted === false).map((todo,index) => {
+                                return (
+                                    <Box key={index} p="2" shadow="md" borderWidth="1px" borderRadius="md" style={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Text>{todo.text}</Text>
+                                        <Checkbox onChange={() => {
+                                            let newTodos = [...todos];
+                                            newTodos[index].isCompleted = !newTodos[index].isCompleted;
+                                            setTodos(newTodos);
+                                        }} />
+                                    </Box>
+                                )
+                            }
+                            ) : todos.filter((todo) => todo.isCompleted === true).map((todo,index) => {
+                                return (
+                                    <Box key={index} p="2" shadow="md" borderWidth="1px" borderRadius="md" style={{ display: "flex", justifyContent: "space-between" }}>
+                                        <Text>{todo.text}</Text>
+                                        <Checkbox onChange={() => {
+                                            let newTodos = [...todos];
+                                            newTodos[index].isCompleted = !newTodos[index].isCompleted;
+                                            setTodos(newTodos);
+                                        }} />
+                                    </Box>
+                                )
+                            }
+                            )
+                        }
+                        
                     </Stack>
                 </CardBody>
+                <FormControl onKeyPress={(e) => onPressEnter(e)}>
+                    <FormLabel>Type Action</FormLabel>
+                    <Input type='text'/>
+                    <Input type='submit'/>
+
+                </FormControl>
             </Card>
         </ChakraProvider>
     );
